310c0680278fea2c856079956e83ddd1386ce421
Alan Chris Dsilva <alanchrisdisilva@gmail.com>
2026-06-07 21:42:40 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != s.length()){
+            return false;
+        }
+
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        
+        return Arrays.equals(sSort,tSort);
+
+        
+
+    }
+}
